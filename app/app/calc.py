af2e5c43cabe89e8b2d737a75e080a485c97bb86
ABC <abc@gmail.com>
2023-05-02 20:22:48 +0530

    Configured dockerfile and created wait_for_db

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,12 +1,14 @@
+# noqa
 """
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add x and y and return result."""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """Subtract x from y and return result."""
     return y - x
-
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -17,26 +17,18 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
-        
+
         call_command('wait_for_db')
-        
+
         patched_check.assert_called_once_with(databases=['default'])
-        
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self,patched_sleep, patched_check):
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-            
+   
         call_command('wait_for_db')
-        
+
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-        
-            
-            
-            
-            
-            
-            
