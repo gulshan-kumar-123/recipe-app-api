394e17d049843429b6c3bd94ea5dc129cd95cd24
ABC <abc@gmail.com>
2023-05-02 20:28:45 +0530

    Configured dockerfile and created wait_for_db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -27,7 +27,7 @@ class CommandTests(SimpleTestCase):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-   
+
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
